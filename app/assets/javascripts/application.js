012433d1ef840c50b718f22d6f78998ca1f1ac43
emcgurty <emcgurty@gmail.com>
2015-03-01 09:01:42 -0500

    App Files Rails 3.2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 /*
 // This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below. DO NOT TOUCH THIS PIECE OF CRAP
+// listed below. DO NOT TOUCH 
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
