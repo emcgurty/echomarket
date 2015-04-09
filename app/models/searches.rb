e1dc42cf2a408cb835d8e1ba7c8b4fb096ee248d
emcgurty <emcgurty2@gmail.com>
2015-04-09 10:07:13 -0400

    Used 1.days to fix date range search

--- app/models/searches.rb
+++ app/models/searches.rb
@@ -1,7 +1,7 @@
 class Searches < ActiveRecord::Base
 
 ##  Thanks to Ryan Bates RailsCast 111
-
+require 'date'
    
    attr_accessible :start_date, :end_date, :keyword, :postal_code, :category_id, :lender_or_borrower, :is_community, :user_id
     
@@ -19,14 +19,14 @@ private
 
 def find_borrowers
 
-  borrowers = Borrowers.find(:all, :readonly, :conditions => getUserType + conditions)
+  borrowers = Borrowers.find(:all, :readonly, :conditions =>  [getUserType, conditions].join(' AND ') )
   
 
 end
 
 def find_lenders
 
-  lenders = Lenders.find(:all, :readonly, :conditions => conditions)
+  lenders = Lenders.find(:all, :readonly, :conditions =>  [getUserType, conditions].join(' AND ') )
 
 end
 
@@ -34,29 +34,37 @@ end
 def getUserType
   
   if is_community == 1  
-       ["user_id = ? AND ", "#{user_id}"]
+       ["user_id = '#{user_id}' "]
   else
-       ["is_active = 1 AND is_community = 0 AND "]
+       ["is_active = 1 AND is_community = 0 "]
   end
 
 end
 
-def keyword_conditions
-   ["item_description like ?  OR item_model like ?", "%#{keyword}%",  "%#{keyword}%"] if keyword.present?
+def description_conditions
+   [" item_description like '%#{keyword}%'"] if keyword.present?
 end
+
+def model_conditions
+   [" item_model like '%#{keyword}%'"] if keyword.present?
+end
+
 
 def category_conditions
   ["item_category_id = #{category_id.to_i}"] if category_id.present?
 end
 
 def date_conditions
-    ["date_created >= ?", "#{start_date}"] if start_date.present? and !end_date.present?
-    ["date_created <= ?", "#{end_date}"] if !start_date.present? and end_date.present?
-    ["( date_created >= ? AND date_created <= ? )", "#{start_date}", "#{end_date}"] if start_date.present? and end_date.present?
+  if end_date.present?
+  @date_plus_one = end_date + 1.days
+  end
+   
+  ["( date_created > '#{start_date}' AND date_created < '#{@date_plus_one.to_s}' )"] if start_date.present? and end_date.present?
+ 
 end
 
 def postalcode_conditions
-  ["postal_code like ?", "#{postal_code}%"]  if postal_code.present?
+  ["postal_code like '#{postal_code}%' "]  if postal_code.present?
 end
 
 def conditions
