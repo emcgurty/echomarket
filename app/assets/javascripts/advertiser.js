7e2046aa56124d972c074d9739f99b2090b9810a
emcgurty <emcgurty2@gmail.com>
2015-05-15 01:07:36 -0400

    Making Advertising progress

--- app/assets/javascripts/advertiser.js
+++ app/assets/javascripts/advertiser.js
@@ -1,60 +1,133 @@
 $(document).ready(function() {
 
+    
+    alert("SADAS");
+    
     $("span.error").css("visibility", "hidden");
 	
+
+	$("#l2bimage").bind('change', function() {
+		$("span#file_image_error").css("visibility", "hidden");
+		$("span#file_image_error").text("");
+
+		var files = !!this.files ? this.files : [];
+		if (!files.length || !window.FileReader) {
+			$("span#file_image_error").text("No file selected? Old browser options?");
+			$("span#file_image_error").css("visibility", "visible");
+			return;
+		}
+
+		if (/^image/.test(files[0].type)) {
+			var reader = new FileReader();
+			reader.readAsDataURL(files[0]);
+
+			reader.onloadend = function() {
+				$("div#image_details").css('display', 'block');
+				$("#imagePreview").attr("src", this.result);
+				$("img#imagePreview_selection").attr("src", this.result);
+
+			}
+		} else {
+			$("span#file_image_error").text("You need to select an image file.");
+			$("span#file_image_error").css("visibility", "visible");
+
+		}
+
+	});
+	$("#l2bimage").trigger('change');
+	
+	$("select#advertiser_advertiser_category_id").bind('change', function() {
+
+		var cat_cmb = $("select#advertiser_advertiser_category_id option:selected").text();
+
+		if (cat_cmb == "Other") {
+
+			$("div#other_category").css("display", "block");
+
+		} else {
+			$("div#other_category").css('display', 'none');
+		}
+
+	});
+
+	$("select#advertiser_advertiser_category_id").trigger('change');
+
 });
 
 function submitAdvertisement(){
 	var foundInvalid = true;
-	var foundInvalidNameCI = true;
-	 
+	
+	$("span.error").text("");
+	$("span.error").css("visibility", "hidden"); 
 	 	
-	if ($("#advertiser_advertiser_name").val() == "") {
-		$("#advertisement_advertiser_name_error").text("Community Name is required.");
-		$("#advertisement_advertiser_name_error").css("visibility", "visible");
+	if ($("#advertiser_title").val() == "") {
+		$("#advertisement_title_error").text("Advertisement title is required.");
+		$("#advertisement_title_error").css("visibility", "visible");
 		foundInvalid = false;
-	} else {
-		$("#advertisement_advertiser_name_error").text("");
-		$("#advertisement_advertiser_name_error").css("visibility", "hidden");
 	}
 	
 	if ($("#advertiser_description").val() == "") {
 		$("#advertisement_description_error").text("Advertisment detail is required.");
 		$("#advertisement_description_error").css("visibility", "visible");
 		foundInvalid = false;
-	} else {
-		$("#advertisement_description_error").text("");
-		$("#advertisement_description_error").css("visibility", "hidden");
-	}
+	} 
 	
 	if ($("#advertiser_advertiser_email").val() == "") {
-		$("#advertisement_advertiser_email_error").text("Advertisement contact email is required.");
-		$("#advertisement_advertiser_email_error").css("visibility", "visible");
+		$("#advertisement_email_error").text("Advertisement contact email is required.");
+		$("#advertisement_email_error").css("visibility", "visible");
 		foundInvalid = false;
-	} else {
-		$("#advertisement_advertiser_email_error").text("");
-		$("#advertisement_advertiser_email_error").css("visibility", "hidden");
-	}
+	} 
 	
-		if ($("#advertiser_advertiser_url").val() == "") {
-		$("#advertisement_advertiser_url_error").text("Advertisement contact URL is required.");
-		$("#advertisement_advertiser_email_error").css("visibility", "visible");
+	if ($("#advertiser_advertiser_url").val() == "") {
+		$("#advertisement_url_error").text("Advertisement contact URL is required.");
+		$("#advertisement_url_error").css("visibility", "visible");
 		foundInvalid = false;
-	} else {
-		$("#advertisement_advertiser_email_error").text("");
-		$("#advertisement_advertiser_email_error").css("visibility", "hidden");
-	}
+	} 
 	
+	var cat_cmb = $("select#advertiser_advertiser_category_id option:selected").text();	
+	if (cat_cmb == 'Please select'){
+		$("#advertiser_category_id_error").text("Advertisement Category is required.");
+		$("#advertiser_category_id_error").css("visibility", "visible");
+		foundInvalid = false;
 		
+	} 
+	
+	var cat_other = $("#advertiser_category_other").text();
+	if (cat_cmb == 'Other' && cat_other == ''){
+		$("#advertiser_category_id_error").text("Other Category information is required.");
+		$("#advertiser_category_id_error").css("visibility", "visible");
+		foundInvalid = false;
+		
+	} 
+	
+	var advert_email = $("#advertiser_advertiser_email").text();
+	if (validEmail(advert_email)){
+		$("#advertisement_email_error").text("Contact Email not properly formatted.");
+		$("#advertisement_email_error").css("visibility", "visible");
+		foundInvalid = false;
+		
+	} 
 	
     if (foundInvalid) {
 
-		$("form").submit();
+		$("form.advertiser").submit();
 	}
 	
 	
 }
 
+
+function validEmail(em) {
+	
+	
+	 var foundIncomplete = false;
+	 var emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+	  if (!( emailRegex.test(em) ) ) {
+	  	foundIncomplete = true;
+	  } 
+	 
+	return foundIncomplete;
+}
 
 
 
--- app/models/advertiser.rb
+++ app/models/advertiser.rb
@@ -1,12 +1,10 @@
 class Advertiser < ActiveRecord::Base
-  # attr_accessible :title, :body
   
-    # attr_accessible :title, :body
   set_primary_key :advertiser_id
+    
   has_many :itemimages
   before_create :get_advertiser_primary_key_value
-  
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
   name_alpha_regex = /\A[ a-zA-Z'-]+\z/
   alpha_numeric_regex = /\A[0-9 a-zA-Z:;.,!?'-]+\z/
   alpha_numeric_regex_msg = "must be alphanumeric characters with typical writing punctuation."
@@ -14,12 +12,13 @@ class Advertiser < ActiveRecord::Base
     
   validates_uniqueness_of :advertiser_name,:if => :advertiser_name, :case_sensitive => true, :message =>  " already exists."
   validates_uniqueness_of :advertiser_email,:if => :advertiser_email, :case_sensitive => false, :message =>  " already exists"
-  validates :advertiser_email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates_uniqueness_of :advertiser_url,:if => :advertiser_url, :case_sensitive => false, :message =>  " already exists"
+  
   
   protected
 
   def get_advertiser_primary_key_value
-     self.advertiser_id = get_random
+     advertiser_id = get_random unless advertiser_id.present?
   end
 
   def get_random
