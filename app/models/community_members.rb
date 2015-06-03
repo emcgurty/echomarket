fc8467dd50d813e6ab4f1c0fc79cbbf3a5aa397c
emcgurty <emcgurty@users.noreply.github.com>
2015-06-03 17:56:24 -0400

    Delete community_members.rb

--- app/models/community_members.rb
+++
@@ -1,9 +0,0 @@
-class CommunityMembers < ActiveRecord::Base
-  
-  
-  set_primary_key :community_member_id
-  attr_accessor :first_name_h,:last_name_h, :mi_h, :alias_h, :community_member_id_h
-  attr_accessible :first_name,:last_name, :mi, :alias, :community_id, :remote_ip, :is_creator, :date_updated, :date_deleted
-  
-  
-end
